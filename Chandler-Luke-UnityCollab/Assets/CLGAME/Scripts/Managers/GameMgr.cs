21dfea3eb610a96529dbdb08446e5cda091875f2
chandlermar <cmartin990@gmail.com>
2022-05-18 17:53:42 -0700

    Terrain Building

---
+++ Chandler-Luke-UnityCollab/Assets/CLGAME/Scripts/Managers/GameMgr.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMgr : MonoBehaviour
+{
+    public static GameMgr inst;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inst = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
