dcf063f2750f91608391a1125f9be1b2b1fbd466
chandlermar <cmartin990@gmail.com>
2022-05-20 01:32:26 -0700

    I need sleep. Also, added a bunny rabbit for fun.

---
+++ Chandler-Luke-UnityCollab/Assets/Rabbits/Demo/Scripts/AnimatorParamatersChange.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+    public class AnimatorParamatersChange : MonoBehaviour
+    {
+
+        private string[] m_buttonNames = new string[] { "Idle", "Run", "Dead" };
+        public Rigidbody bunnyRB;
+        private Animator m_animator;
+
+        // Use this for initialization
+        void Start()
+        {
+
+            m_animator = GetComponent<Animator>();
+            bunnyRB = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (this.tag == "Male")
+            {
+                m_animator.SetTrigger("Next");
+                m_animator.SetTrigger("Next");
+            }    
+        }
+
+        private void OnGUI()
+        {
+            //GUI.BeginGroup(new Rect(0, 0, 150, 1000));
+
+                    //for (int i = 0; i < m_buttonNames.Length; i++)
+                    //{
+                    //if (GUILayout.Button(m_buttonNames[i], GUILayout.Width(150)))
+                    //{
+                    //m_animator.SetInteger("AnimIndex", 2);
+                    //m_animator.SetTrigger("Next");
+                    
+            //}
+            // }
+
+            //GUI.EndGroup();
+        }
+    }
+
